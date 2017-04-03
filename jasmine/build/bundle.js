3dc89800b08ecbb97b8e437b0ce3c463032a8197
Imisioluwa Akande <imisioluwa.akande@andela.com>
2017-04-03 18:10:29 +0100

    Modification suceeded

--- jasmine/build/bundle.js
+++ jasmine/build/bundle.js
@@ -12,9 +12,6 @@ module.exports=books = [
 ]
 
 },{}],2:[function(require,module,exports){
-module.exports=bookempty = []
-
-},{}],3:[function(require,module,exports){
 module.exports=books3 = [
   {
     "title": "Alice in Wonderland",
@@ -31,7 +28,128 @@ module.exports=books3 = [
   }
 ]
 
+},{}],3:[function(require,module,exports){
+module.exports=bookempty = []
+
 },{}],4:[function(require,module,exports){
+module.exports=notBook = {
+  "title": "hello",
+  "text": "hello"
+}
+},{}],5:[function(require,module,exports){
+'use strict';
+
+var _book = require('./../book.json');
+
+var _book2 = _interopRequireDefault(_book);
+
+var _emptyBook = require('./../emptyBook.json');
+
+var _emptyBook2 = _interopRequireDefault(_emptyBook);
+
+var _wrongFormat = require('./../wrongFormat.json');
+
+var _wrongFormat2 = _interopRequireDefault(_wrongFormat);
+
+var _invalidBook = require('./../invalidBook.json');
+
+var _invalidBook2 = _interopRequireDefault(_invalidBook);
+
+var _books = require('./../books.json');
+
+var _books2 = _interopRequireDefault(_books);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var myInvertedIndex = new InvertedIndexClass();
+
+/* Test Suites */
+describe('Inverted Index Test', function () {
+  describe('ReadFile', function () {
+    beforeEach(function () {
+      myInvertedIndex.files['book.json'] = _book2.default;
+      myInvertedIndex.files['books.json'] = _books2.default;
+    });
+    it('should return false when checking an empty JSON array', function () {
+      expect(myInvertedIndex.readFile(_emptyBook2.default)).toBeFalsy();
+    });
+    it('should return false when checking a wrongformatted JSON array', function () {
+      expect(myInvertedIndex.readFile(_wrongFormat2.default)).toBeFalsy();
+    });
+    it('should return false when book is not an array', function () {
+      expect(myInvertedIndex.readFile(_invalidBook2.default)).toBeFalsy();
+    });
+    it('should return true if a valid JSON array', function () {
+      expect(myInvertedIndex.readFile(_book2.default)).toBeTruthy();
+    });
+
+    it('should return correct keys for files when file is saved', function () {
+      expect(Object.keys(myInvertedIndex.files)).toEqual(['book.json', 'books.json']);
+    });
+
+    it('should ensure the file content is saved accurrately', function () {
+      expect(myInvertedIndex.files['book.json']).toEqual(_book2.default);
+    });
+  });
+  describe('Validate File', function () {
+    it('should return false when file does not contain "title" & "text" format', function () {
+      expect(myInvertedIndex.validateFile(_wrongFormat2.default)).toBeFalsy();
+    });
+    it('verifies that the JSON file is valid', function () {
+      expect(myInvertedIndex.validateFile(_book2.default).length).toEqual(2);
+    });
+  });
+  describe('Create Index Table', function () {
+    beforeEach(function () {
+      myInvertedIndex.files['books.json'] = _books2.default;
+      myInvertedIndex.files['book.json'] = _book2.default;
+    });
+    it('should ensure that index is created', function () {
+      expect(myInvertedIndex.createIndex('book.json')).toBeTruthy();
+    });
+    it('should ensure that index is created', function () {
+      expect(myInvertedIndex.createIndex('books.json')).toBeTruthy();
+    });
+  });
+  describe('Get Index', function () {
+    it('should ensure that index of a file is returned accurrately', function () {
+      expect(myInvertedIndex.getIndex('book.json').alice).toEqual([0]);
+    });
+  });
+  describe('Tokenize', function () {
+    beforeEach(function () {
+      myInvertedIndex.files['books.json'] = _books2.default;
+    });
+    it('should return correct terms in form of string in an array', function () {
+      expect(myInvertedIndex.tokenize(_books2.default[0].text)).toEqual(['alice', 'falls', 'into', 'a', 'rabbit', 'hole', 'and', 'enters', 'a', 'world', 'full', 'of', 'imagination']);
+    });
+    it('should return correct terms in terms of lowercase', function () {
+      expect(myInvertedIndex.tokenize(_books2.default[0].text)[0]).toEqual('alice');
+    });
+    it('should return array of terms without space or invalid characters', function () {
+      expect(myInvertedIndex.tokenize('alice.,&-falls')).toEqual(['alice', 'falls']);
+    });
+  });
+  describe('Search Index', function () {
+    beforeEach(function () {
+      myInvertedIndex.files['books.json'] = _books2.default;
+    });
+    it('should return correct index of the search term in all books', function () {
+      expect(myInvertedIndex.searchIndex('alice')).toEqual({ 'book.json': { alice: [0] }, 'books.json': { alice: [0] } });
+    });
+    it('should return correct index of the search term in books3.json', function () {
+      expect(myInvertedIndex.searchIndex('an alice', ['book.json', 'books.json'])).toEqual({ 'book.json': { an: [1], alice: [0] },
+        'books.json': { an: [1, 2], alice: [0] } });
+    });
+    it('should return false when no result is found', function () {
+      expect(myInvertedIndex.searchIndex('impossibility')).toBeFalsy();
+    });
+    it('should return true when the result is found', function () {
+      expect(myInvertedIndex.searchIndex('impossibility')).toBeFalsy();
+    });
+  });
+});
+},{"./../book.json":1,"./../books.json":2,"./../emptyBook.json":3,"./../invalidBook.json":4,"./../wrongFormat.json":6}],6:[function(require,module,exports){
 module.exports=bookwrongformat = [
   {
     "what": "Alice in Wonderland",
@@ -44,106 +162,4 @@ module.exports=bookwrongformat = [
   }
 ]
 
-},{}],5:[function(require,module,exports){
-module.exports=notBook = {
-  "title": "hello",
-  "text": "hello"
-}
-},{}],6:[function(require,module,exports){
-'use strict';
-
-var _book = require('./../book.json');
-
-var _book2 = _interopRequireDefault(_book);
-
-var _bookempty = require('./../bookempty.json');
-
-var _bookempty2 = _interopRequireDefault(_bookempty);
-
-var _bookwrongformat = require('./../bookwrongformat.json');
-
-var _bookwrongformat2 = _interopRequireDefault(_bookwrongformat);
-
-var _notBook = require('./../notBook.json');
-
-var _notBook2 = _interopRequireDefault(_notBook);
-
-var _books = require('./../books3.json');
-
-var _books2 = _interopRequireDefault(_books);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var myInvertedIndex = new InvertedIndexClass(); /* global InvertedIndexClass */
-
-myInvertedIndex.files['book.json'] = _book2.default;
-myInvertedIndex.files['books3.json'] = _books2.default;
-
-/* Test Suites */
-describe('Inverted Index Test', function () {
-  describe('ReadFile', function () {
-    it('should return false when checking an empty JSON array', function () {
-      expect(myInvertedIndex.readFile(_bookempty2.default)).toBeFalsy();
-    });
-    it('should return false when checking an wrongformatted JSON array', function () {
-      expect(myInvertedIndex.readFile(_bookwrongformat2.default)).toBeFalsy();
-    });
-    it('should return false when book is not an array', function () {
-      expect(myInvertedIndex.readFile(_notBook2.default)).toBeFalsy();
-    });
-    it('should return true if a valid JSON array', function () {
-      expect(myInvertedIndex.readFile(_book2.default)).toBeTruthy();
-    });
-
-    it('should return correct keys for files when file is saved', function () {
-      expect(Object.keys(myInvertedIndex.files)).toEqual(['book.json', 'books3.json']);
-    });
-
-    it('should ensure the file content is saved accurrately', function () {
-      expect(myInvertedIndex.files['book.json']).toEqual(_book2.default);
-    });
-  });
-  describe('Validate File', function () {
-    it('should return false when file does not contain "title" & "text" format', function () {
-      expect(myInvertedIndex.validateFile(_bookwrongformat2.default)).toBeFalsy();
-    });
-  });
-  it('verifies that the JSON file is valid', function () {
-    expect(myInvertedIndex.validateFile(_book2.default).length).toEqual(2);
-  });
-  describe('Create Index Table', function () {
-    it('should ensure that index is created', function () {
-      expect(myInvertedIndex.createIndex('book.json')).toBeTruthy();
-    });
-    it('should ensure that index is created', function () {
-      expect(myInvertedIndex.createIndex('books3.json')).toBeTruthy();
-    });
-  });
-  describe('Get Index', function () {
-    it('should ensure that index of a file is returned accurrately', function () {
-      expect(myInvertedIndex.getIndex('book.json').alice).toEqual([0]);
-    });
-  });
-  describe('Tokenize', function () {
-    it('should return correct terms in form of string in an array', function () {
-      expect(myInvertedIndex.tokenize(_books2.default[0].text)).toEqual(['alice', 'falls', 'into', 'a', 'rabbit', 'hole', 'and', 'enters', 'a', 'world', 'full', 'of', 'imagination']);
-    });
-  });
-
-  describe('Search Index', function () {
-    it('should return correct index of the search term in all books', function () {
-      expect(myInvertedIndex.searchIndex('alice')).toEqual({ 'book.json': { alice: [0] }, 'books3.json': { alice: [0] } });
-    });
-    it('should return correct index of the search term in books3.json', function () {
-      expect(myInvertedIndex.searchIndex('an alice', ['book.json', 'books3.json'])).toEqual({ 'book.json': { an: [1], alice: [0] },
-        'books3.json': { an: [1, 2], alice: [0] } });
-    });
-    it('should return false when no result is found', function () {
-      expect(myInvertedIndex.searchIndex('impossibility')).toBeFalsy();
-    });
-    it('should return true when the result is found', function () {
-      expect(myInvertedIndex.searchIndex('impossibility')).toBeFalsy();
-    });
-  });
-});
-},{"./../book.json":1,"./../bookempty.json":2,"./../books3.json":3,"./../bookwrongformat.json":4,"./../notBook.json":5}]},{},[6])
+},{}]},{},[5])
